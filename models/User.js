8e912d420af5108a29cffa0a2cf8f47ec1bca085
AyloSrd <aylo.srd@gmail.com>
2020-09-28 09:45:43 +0200

    se

---
+++ models/User.js
@@ -0,0 +1,17 @@
+/*
+email
+password
+first name
+last name
+adresse
+telephone
+info: {
+	id-lenses
+	frequency : enum
+	reminder
+	delivered: true false
+	carte vitale: cloudinary
+	mutuelle: cloudinary
+	ordonnance: cloudinary
+}
+*/
