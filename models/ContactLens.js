8f93bd90913a8ae0902aa28a792c790ab72abf76
AyloSrd <aylo.srd@gmail.com>
2020-09-28 12:32:47 +0200

    Lens model created

--- models/ContactLens.js
+++ models/ContactLens.js
@@ -1,5 +1,12 @@
-/*
-img 
-brand
-description
-*/
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const LensSchema = new Schema({
+	img:String,
+	brand:String
+})
+
+const Lens = mongoose.model('Lens', LensSchema)
+
+module.exports = Lens
+
