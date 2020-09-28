9b879ac5a3bac5b5aeed8dece746ba6f9772e122
AyloSrd <aylo.srd@gmail.com>
2020-09-28 17:21:35 +0200

    se

--- models/User.js
+++ models/User.js
@@ -10,19 +10,14 @@ const userSchema = new Schema({
   addresse: String,
   telephone: Number,
   info: {
-	id_ContactLens: { type: Schema.Types.ObjectId, ref: "Lens" },
-<<<<<<< HEAD
-	frequency : {type:String, enum :["daily","weekly","monthly"], default: "daily"},
-=======
-	frequency : {type: String, enum :["daily","weekly","montly"]},
->>>>>>> fff332dbcab29b0c0559c97e8dbfa0994e268d62
-	reminder : Date,
-	delivered : Boolean,
-	carteVital: String,
-	mutuelle : String,
-	ordonnance: String,
-	
-},
+    id_ContactLens: { type: Schema.Types.ObjectId, ref: "Lens" },
+    frequency : {type:String, enum :["daily","weekly","monthly"], default: "daily"},
+    reminder : Date,
+    delivered : Boolean,
+    carteVital: String,
+    mutuelle : String,
+    ordonnance: String,
+  },
 })
 
 const User = mongoose.model("User", userSchema);
