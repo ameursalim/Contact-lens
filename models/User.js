7913831cb783869db0245dab76729b22169d0689
salim ameur <salim.ameur@gmail.com>
2020-09-28 12:46:26 +0200

    user model done

--- models/User.js
+++ models/User.js
@@ -1,3 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["admin", "user"], default: "user" },
+  avatar: String,
+  adresse: String,
+  telephone: Number,
+  info: {
+	id_ContactLens: { type: Schema.Types.ObjectId, ref: "Lens" },
+	frequency : {enum :["daily","weekly","montly"]},
+	reminder : Date,
+	delivered : Boolean,
+	carte_vital: String,
+	mutuelle : String,
+	ordonnance: String,
+
+	
+},
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
 /*
 email
 password
