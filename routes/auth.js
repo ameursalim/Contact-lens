7e97d896e7b6923bfbe2f20bd7ed328841f3a391
salim ameur <salim.ameur@gmail.com>
2020-09-28 12:11:24 +0200

    sign up done

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,9 @@ router.get("/signin", async (req, res, next) => {
   res.render("auth/signin.hbs");
 });
 
+router.get("/signup", async (req, res, next) => {
+    res.render("auth/signup.hbs");
+  });
+  
+
 module.exports = router;
