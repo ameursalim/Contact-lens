a1e46985bc09ec2b897472473fa3e28727d1921b
salim ameur <salim.ameur@gmail.com>
2020-09-29 16:46:47 +0200

    signin done

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
   telephone: Number,
   info: {
     id_ContactLens: { type: Schema.Types.ObjectId, ref: "Lens" },
-    frequency : {type:String, enum :["daily","weekly","monthly"], default: "daily"},
+    frequency : {type:String, enum :["daily","weekly","monthly"]},
     reminder : Date,
     delivered : Boolean,
     carteVitale: String,
